35b560fb2c419f723e7d08c15eaa5b2a0f2a51d9
YashiAg26 <92780959+YashiAg26@users.noreply.github.com>
2024-02-14 16:29:41 +0530

    Update DisableWindowsFirewall.ps1

--- Azure-VM-Windows1/scripts/DisableWindowsFirewall.ps1
+++ Azure-VM-Windows1/scripts/DisableWindowsFirewall.ps1
@@ -3,3 +3,5 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Par
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -name "EnableFirewall" -Value 0
 
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\Standardprofile' -name "EnableFirewall" -Value 0
+
+Restart-AzVM
