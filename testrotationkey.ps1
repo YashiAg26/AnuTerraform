f1fe1ec2e390b79dfcf9823a5b0ff18a2a7efcb1
YashiAg26 <92780959+YashiAg26@users.noreply.github.com>
2024-02-26 19:47:36 +0530

    Update testrotationkey.ps1

--- testrotationkey.ps1
+++ testrotationkey.ps1
@@ -1,3 +1,26 @@
+Connect-AzAccount
+Set-AzContext -SubscriptionId cbf8b966-9488-4fc7-a133-b65c3b0285a8
+$resourceGroupName = "firewallrg"
+$storageAccountName = "testingstglv"
+$keyVaultName = "testingkvlv"
+$keyVaultSpAppId = "cfa8b339-82a2-471a-a3c9-0fc0be7a4093"
+$storageAccountKey = "key1"
+
+$userId = "admin@Mindtree226.onmicrosoft.com"
+$storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName
+
+New-AzRoleAssignment -ApplicationId $keyVaultSpAppId -RoleDefinitionName 'Storage Account Key Operator Service Role' -Scope $storageAccount.Id
+
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -UserPrincipalName $userId -PermissionsToStorage get, list, delete, set, update, regeneratekey, getsas, listsas, deletesas, setsas, recover, backup, restore, purge
+
+Add-AzKeyVaultManagedStorageAccount -VaultName $keyVaultName -AccountName $storageAccountName -AccountResourceId $storageAccount.Id -ActiveKeyName $storageAccountKey -DisableAutoRegenerateKey
+
+$regenPeriod = [System.Timespan]::FromDays(1)
+
+Add-AzKeyVaultManagedStorageAccount -VaultName $keyVaultName -AccountName $storageAccountName -AccountResourceId $storageAccount.Id -ActiveKeyName $storageAccountKey -RegenerationPeriod $regenPeriod
+
+
+$userId = (Get-AzContext).Account.Id
 $resourceGroupName = "firewallrg"
 $storageAccountName = "testingstglv"
 $keyVaultName = "testingkvlv"
